a225966eea7b943c02583d84ecc2a43fac018dd0
ritam <38151929+rit-am@users.noreply.github.com>
2020-08-14 07:51:14 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,24 @@ const { Telegraf } = require('telegraf')
 const bot = new Telegraf(BOT_TOKEN)
 const bodyParser = require('body-parser')
 
+
 // Start Coding
 bot.start((ctx) => ctx.reply('Hey, wellcome!'))
 
-bot.command('about', (ctx) => {ctx.reply(`Telegram Bot!`)})
+bot.command('about', (ctx) => {ctx.reply(`Service Bot created by  @ritam_mukherjee!`)})
+
+bot.command('help', (ctx) => {ctx.reply(`Supported Commands \n about \n help \n whereami \n ip \n radiosg_next`)})
+
+bot.command('whereami', (ctx) => { 
+    require('http').get('http://ip-api.com/json/', (res) => {
+        res.setEncoding('utf8');
+        res.on('data', function (body) {console.log(body);
+            var jsonParsed = JSON.parse(body);
+            ctx.reply(jsonParsed.city+", "+jsonParsed.regionName+", "+jsonParsed.country);
+        //ctx.reply(body)
+        });                
+    });
+})
 
 bot.command('ip', (ctx) => { 
     require('http').get('http://httpbin.org/ip', (res) => {
